02d4152af371358ff911d1902e735ef26a9de9b9
REHAN AHMAD <rehanahmad.khan9198@gmail.com>
2026-04-18 10:02:21 +0530

    Add files via upload

---
+++ js_gcf/dom2.js
@@ -0,0 +1,10 @@
+var myh1=document.querySelector('h1');
+
+myh1.style.color='BLue'
+myh1.style.backgroundColor='lightgreen'
+myh1.style.textAlign='center'
+var link1=document.querySelector('a')
+link1.style.textDecoration='none'
+const myh2 = document.querySelector('h1');
+myh2.addEventListener('click', function(e){});
+e.target.removeEventListener('click', arguments.callee);
